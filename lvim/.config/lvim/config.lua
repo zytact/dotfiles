b44909e02e7508b2ffe1de3323b0a761acef9d2d
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-05-05 18:16:29 +0530

    adding keybindings for moving blocks of code

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -23,6 +23,9 @@ lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
 
+-- For moving(not cutting and pasting) blocks of code at once
+lvim.keys.visual_mode["J"] = ":m '>+1<CR>gv=gv"
+lvim.keys.visual_mode["K"] = ":m '<-2<CR>gv=gv"
 -- -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["t"] = { "<cmd>ToggleTerm<CR>", "ToggleTerm" }
 lvim.builtin.which_key.mappings["gf"] = { "<cmd>Telescope git_files<CR>", "Find Git Files" }
