5143fc6767d0cb1115dfdcb0d863c89f9771f55d
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-05-05 23:38:28 +0530

    adding undofile save

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -3,6 +3,8 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
 
 -- general lvim options
 lvim.log.level = "info"
