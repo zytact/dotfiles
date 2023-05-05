b6290e2f94f51687ac4197d6bc16b63be34739df
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-05-05 23:39:46 +0530

    adding undotree toggle keymap

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -32,6 +32,7 @@ lvim.keys.visual_mode["K"] = ":m '<-2<CR>gv=gv"
 -- -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["t"] = { "<cmd>ToggleTerm<CR>", "ToggleTerm" }
 lvim.builtin.which_key.mappings["gf"] = { "<cmd>Telescope git_files<CR>", "Find Git Files" }
+lvim.builtin.which_key.mappings["U"] = { "<cmd>UndotreeToggle<CR>", "Toggle UndoTree" }
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
