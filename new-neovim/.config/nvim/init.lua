7e8a507ab1c9093c82510fef1205e15c6e1b8cd7
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-06-01 10:22:19 +0530

    mapping <leader>r to rename symbol

--- new-neovim/.config/nvim/init.lua
+++ new-neovim/.config/nvim/init.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = ","
 
 
 vim.api.nvim_set_keymap('n', 'gd', '<Cmd>call VSCodeNotify("editor.action.revealDefinition")<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>call VSCodeNotify("editor.action.rename")<CR>', { noremap = true })
