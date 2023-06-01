a695444decfc3970f90682648a9e28c6acf73c0e
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-06-01 10:23:23 +0530

    removing onedarkpro and adding onedark

--- lvim/.config/lvim/lua/plugins.lua
+++ lvim/.config/lvim/lua/plugins.lua
@@ -1,11 +1,8 @@
 -- -- Additional Plugins <https://www.lunarvim.org/docs/configuration/plugins/user-plugins>
 lvim.plugins = {
-  { "catppuccin/nvim",     name = "catppuccin" },
-  { "Mofiqul/vscode.nvim", name = "vscode-dark" },
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000 -- Ensure it loads first
-  },
+  { "catppuccin/nvim",         name = "catppuccin" },
+  { "Mofiqul/vscode.nvim",     name = "vscode-dark" },
+  { "navarasu/onedark.nvim" },
   { 'rose-pine/neovim',        name = 'rose-pine' },
   { "f-person/git-blame.nvim", name = 'git-blame' },
   { "mbbill/undotree",         name = 'undotree' },
