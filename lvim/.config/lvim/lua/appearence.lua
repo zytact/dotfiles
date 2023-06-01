9650388d0e9fde1235b393f00722a07935e37441
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-06-01 10:24:02 +0530

    setting onedark as default theme

--- lvim/.config/lvim/lua/appearence.lua
+++ lvim/.config/lvim/lua/appearence.lua
@@ -1,6 +1,6 @@
 -- -- Change theme settings
-lvim.colorscheme = "ayu-mirage"
-lvim.transparent_window = true
+lvim.colorscheme = "onedark"
+-- lvim.transparent_window = false
 lvim.builtin.lualine.style = "lvim"
 
 -- Change other appearence settings
@@ -9,3 +9,16 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- Onedark
+require('onedark').setup {
+  style = 'deep',
+  transparent = true,
+  code_style = {
+    comments = 'italic',
+    keywords = 'bold',
+    functions = 'bold',
+    strings = 'italic',
+    variables = 'none'
+  }
+}
