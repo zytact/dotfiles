088f91e1155f748914106427c328cc74e809deee
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-06-08 15:35:26 +0530

    adding keybinding for vscode explorer

--- new-neovim/.config/nvim/init.lua
+++ new-neovim/.config/nvim/init.lua
@@ -4,3 +4,5 @@ vim.g.mapleader = ","
 vim.api.nvim_set_keymap('n', 'gd', '<Cmd>call VSCodeNotify("editor.action.revealDefinition")<CR>', { noremap = true })
 
 vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>call VSCodeNotify("editor.action.rename")<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>e', '<Cmd>call VSCodeNotify("workbench.view.explorer")<CR>', { noremap = true })
