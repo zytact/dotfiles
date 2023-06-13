047b9fa0aa86f307fc4702520bb76a87983b97c1
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-06-13 13:39:59 +0530

    formatting

--- lvim/.config/lvim/lua/plugins.lua
+++ lvim/.config/lvim/lua/plugins.lua
@@ -1,27 +1,27 @@
 -- -- Additional Plugins <https://www.lunarvim.org/docs/configuration/plugins/user-plugins>
 lvim.plugins = {
-  { "catppuccin/nvim",         name = "catppuccin" },
-  { "Mofiqul/vscode.nvim",     name = "vscode-dark" },
-  { "navarasu/onedark.nvim" },
-  { 'rose-pine/neovim',        name = 'rose-pine' },
-  { "f-person/git-blame.nvim", name = 'git-blame' },
-  { "mbbill/undotree",         name = 'undotree' },
-  {
-    'Saecki/crates.nvim',
-    name = 'crates',
-    dependencies = {
-      'nvim-lua/plenary.nvim'
+    { "catppuccin/nvim",         name = "catppuccin" },
+    { "Mofiqul/vscode.nvim",     name = "vscode-dark" },
+    { "navarasu/onedark.nvim" },
+    { 'rose-pine/neovim',        name = 'rose-pine' },
+    { "f-person/git-blame.nvim", name = 'git-blame' },
+    { "mbbill/undotree",         name = 'undotree' },
+    {
+        'Saecki/crates.nvim',
+        name = 'crates',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        },
+        config = function()
+            require('crates').setup()
+        end
     },
-    config = function()
-      require('crates').setup()
-    end
-  },
-  {
-    'norcalli/nvim-colorizer.lua',
-    name = 'nvim-colorizer',
-    config = function()
-      require('colorizer').setup()
-    end
-  },
-  { "Shatur/neovim-ayu" }
+    {
+        'norcalli/nvim-colorizer.lua',
+        name = 'nvim-colorizer',
+        config = function()
+            require('colorizer').setup()
+        end
+    },
+    { "Shatur/neovim-ayu" },
 }
