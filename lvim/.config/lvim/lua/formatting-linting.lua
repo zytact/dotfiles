aa30fec8de5ea8e16bc463b1e7e8266eb3b71342
Arnab Chakraborty <50040499+iam-arnab@users.noreply.github.com>
2023-06-13 13:44:50 +0530

    update

--- lvim/.config/lvim/lua/formatting-linting.lua
+++ lvim/.config/lvim/lua/formatting-linting.lua
@@ -1,35 +1,35 @@
 lvim.format_on_save = {
-  enabled = true,
-  pattern = { "*.lua", "*.rs", "*.py", "*.js", ".ts", "*.cpp" },
-  timeout = 1000,
+    enabled = true,
+    pattern = { "*.lua", "*.rs", "*.py", "*.js", ".ts", "*.cpp" },
+    timeout = 1000,
 }
 
 -- -- linters, formatters and code actions <https://www.lunarvim.org/docs/configuration/language-features/linting-and-formatting>
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-  {
-    name = "rustfmt",
-    filetypes = { "rust" }
-  },
-  {
-    name = "autopep8",
-    filetypes = { "python" }
-  }
-  -- { command = "stylua" },
-  --   {
-  --     command = "prettier",
-  --     extra_args = { "--print-width", "100" },
-  --     filetypes = { "typescript", "typescriptreact" },
-  --   },
+    {
+        name = "rustfmt",
+        filetypes = { "rust" }
+    },
+    {
+        name = "autopep8",
+        filetypes = { "python" }
+    },
+    {
+        name = "prettier",
+        extra_args = { "--print-width", "100" },
+        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" }
+    },
+    -- { command = "stylua" },
 }
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  -- { command = "flake8", filetypes = { "python" } },
-  {
-    command = "shellcheck",
-    args = { "--severity", "warning" },
-    filetypes = { "sh" }
-  },
+    -- { command = "flake8", filetypes = { "python" } },
+    {
+        command = "shellcheck",
+        args = { "--severity", "warning" },
+        filetypes = { "sh" }
+    },
 }
 -- local code_actions = require "lvim.lsp.null-ls.code_actions"
 -- code_actions.setup {
