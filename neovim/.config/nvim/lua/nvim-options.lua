1b2ca27d9d9d1ac23f9b497439dddc07c52cefc2
Arnab Chakraborty <iam-arnab@outlook.com>
2023-07-29 20:42:50 +0530

    turning on number line

--- neovim/.config/nvim/lua/nvim-options.lua
+++ neovim/.config/nvim/lua/nvim-options.lua
@@ -2,6 +2,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
+vim.opt.nu = true
 vim.opt.smartindent = true
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
