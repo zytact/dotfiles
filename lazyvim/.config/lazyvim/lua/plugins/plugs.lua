5b42536a1580b05cd5feccc61c9a81f578367b5a
Arnab <arnab@pop-os.localdomain>
2024-08-04 22:40:49 +0530

    change keymap for supermaven change accept

--- lazyvim/.config/lazyvim/lua/plugins/plugs.lua
+++ lazyvim/.config/lazyvim/lua/plugins/plugs.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       require("supermaven-nvim").setup({
         keymaps = {
-          accept_suggestion = "<C-y>",
+          accept_suggestion = "<C-a>",
         },
       })
       require("supermaven-nvim.api").use_free_version()
