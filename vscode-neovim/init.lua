74dd69d65be465ccb24fba833dd2855661206b80
Arnab <arnab@pop-os.localdomain>
2024-08-08 00:50:01 +0530

    refactor

--- vscode-neovim/init.lua
+++ vscode-neovim/init.lua
@@ -1,8 +1,43 @@
-vim.g.mapleader = ","
+vim.g.mapleader = " "
 
+vim.api.nvim_set_keymap(
+	"n",
+	"gd",
+	'<Cmd>lua require("vscode").call("editor.action.revealDefinition")<CR>',
+	{ noremap = true }
+)
 
-vim.api.nvim_set_keymap('n', 'gd', '<Cmd>call VSCodeNotify("editor.action.revealDefinition")<CR>', { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>r",
+	'<Cmd>lua require("vscode").call("editor.action.rename")<CR>',
+	{ noremap = true }
+)
 
-vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>call VSCodeNotify("editor.action.rename")<CR>', { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>e",
+	'<Cmd>lua require("vscode").call("workbench.view.explorer")<CR>',
+	{ noremap = true }
+)
 
-vim.api.nvim_set_keymap('n', '<leader>e', '<Cmd>call VSCodeNotify("workbench.view.explorer")<CR>', { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>c",
+	'<Cmd>lua require("vscode").call("workbench.panel.chat.view.copilot.focus")<CR>',
+	{ noremap = true }
+)
+
+vim.api.nvim_set_keymap(
+	"n",
+	"gt",
+	'<Cmd>lua require("vscode").call("workbench.action.showAllSymbols")<CR>',
+	{ noremap = true }
+)
+
+vim.api.nvim_set_keymap(
+	"n",
+	"gs",
+	'<Cmd>lua require("vscode").call("workbench.action.gotoSymbol")<CR>',
+	{ noremap = true }
+)
