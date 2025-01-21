276e5df04028abfb0930378656ee3f53d28d6bbc
Arnab Chakraborty <iam-arnab@outlook.com>
2025-01-21 23:58:12 +0530

    update

--- lazyvim/.config/lazyvim/lua/plugins/colorscheme.lua
+++ lazyvim/.config/lazyvim/lua/plugins/colorscheme.lua
@@ -120,9 +120,14 @@ return {
     end,
   },
   {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+  },
+
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "onedark",
     },
   },
 }
