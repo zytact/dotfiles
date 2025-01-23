2a941083be099f28dbe8e68181fd3ff09c04719a
Arnab Chakraborty <iam-arnab@outlook.com>
2025-01-24 00:19:52 +0530

    add avante.nvim and replace CopilotChat.nvim

--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -2,5 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local wk = require("which-key")
+wk.add({
+  { "<leader>a", group = "ai", icon = "  " },
+})
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { noremap = true, silent = true })
