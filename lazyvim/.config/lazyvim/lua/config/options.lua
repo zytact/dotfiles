47d16faaf011c14d21a456bf29d08298cf440733
Arnab Chakraborty <iam-arnab@outlook.com>
2025-05-12 18:09:32 +0530

    enable light theme, termguicolors and eob

--- lazyvim/.config/lazyvim/lua/config/options.lua
+++ lazyvim/.config/lazyvim/lua/config/options.lua
@@ -11,8 +11,11 @@ vim.o.expandtab = true
 vim.o.cursorline = false
 vim.g.autoformat = true
 vim.o.exrc = true
+vim.o.termguicolors = true
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
+vim.opt.fillchars = { eob = "~" }
+vim.o.background = "light"
 
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font:h12"
