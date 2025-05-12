83776883bae2ee25e12dd47769c769e5eec4be2a
Arnab Chakraborty <iam-arnab@outlook.com>
2025-05-12 18:13:46 +0530

    pin mason until update

--- lazyvim/.config/lazyvim/lua/plugins/plugs.lua
+++ lazyvim/.config/lazyvim/lua/plugins/plugs.lua
@@ -266,4 +266,11 @@ return {
       })
     end,
   },
+  -- mason and mason-lspconfig released version 2.0.
+  -- with some breaking changes, multiple methods have been changed
+  -- therefore (for now) a workaround is needed for Mason to still work in LazyVim
+  -- THIS WILL PIN the VERSION number, remove this file later, when it's no longer needed
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+  -- after adding/saving this file run :Lazy to potentially 're-install' the versions above
 }
